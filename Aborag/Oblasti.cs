776644f306fe80e498f2866f070366cfb2a84e82
Správce <Sprvce>
2017-03-07 17:48:12 +0100

    uvodni projekt

---
+++ Aborag/Oblasti.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aborag
+{
+    public class Oblasti
+    {
+        int[,,] vojacioblasti;
+        int[] podminky;
+        int[] kamen;
+
+
+        public Oblasti()
+        {
+            vojacioblasti = new int[9,9,2];
+            podminky = new int[9];
+            kamen = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                podminky[i] = 1; //zmenit
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                kamen[i] = 20;
+            }
+
+
+        }
+
+        public void pridejjednotky(int oblast, int typ, int pocet, int hrac)
+        {
+            vojacioblasti[oblast, typ, hrac] += pocet;
+        }
+
+        public void odeberjednotky(int oblast, int typ, int pocet, int hrac)
+        {
+            vojacioblasti[oblast, typ, hrac] -= pocet;
+        }
+
+        public void vytezkamen(int oblast)
+        {
+            kamen[oblast]--;
+        }
+
+        public int ziskejpocet(int oblast, int typ, int hrac)
+        {
+            return vojacioblasti[oblast, typ, hrac];
+        }
+
+        public int ziskejpodminky(int oblast)
+        {
+            return podminky[oblast];
+        }
+    }
+}
---
+++ Aborag/Souboj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aborag
+{
+    class Souboj
+    {
+        int pocethrac0;
+        int pocethrac1;
+        int podminky;
+        Oblasti oblasti;
+        
+
+        public Souboj(int oblast)
+        {
+            oblasti = Game1.oblasti;
+            podminky = oblasti.ziskejpodminky(oblast);
+            for (int i = 0; i < 9; i++)
+            {
+                pocethrac0 = oblasti.ziskejpocet(/*oblast*/ oblast, /*typ*/ i, /*hrac*/ 0);
+                pocethrac1 = oblasti.ziskejpocet(/*oblast*/ oblast, /*typ*/ i, /*hrac*/ 1);
+            }
+
+        }
+    }
+}
